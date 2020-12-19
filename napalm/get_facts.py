fef3d4654d2ac86cb0542e4e806483c7fc7fba98
shrutikaponde <pondeshrutika@gmail.com>
2020-12-19 15:21:35 +0530

    Added get_facts napalm example

---
+++ napalm/get_facts.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3.6
+
+# 
+# Connecting to cisco router configured in GNS3
+# 
+
+from napalm import get_network_driver
+from pprint import pprint
+
+driver = get_network_driver('ios')
+conn_method = {'transport': 'telnet', 'secret': 'cisco'}
+host = '192.168.122.146'  # your ip address
+user = ''
+passwd = 'cisco'
+
+# With context manager
+with driver(hostname=host, username=user, password=passwd, optional_args=conn_method ) as device:
+    print('Getting facts')
+    pprint(device.get_facts())
