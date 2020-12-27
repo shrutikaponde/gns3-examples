08bf6cd01d28720b412fae301e6d0669af0fa24e
shrutik@p <pondeshrutika@gmail.com>
2020-12-27 18:24:41 +0530

    Added ubuntu installation script

---
+++ install.sh
@@ -0,0 +1,12 @@
+git clone https://github.com/shrutikaponde/gns3-examples.git
+cd gns3-examples/
+git checkout ubuntu-gns3-topology
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.9 -y
+sudo apt install python3.9-dev python3.9-venv -y
+python3.9 -m venv .nornir-venv
+. .nornir-venv/bin/activate
+pip install nornir nornir_utils nornir_napalm nornir_jinja2
